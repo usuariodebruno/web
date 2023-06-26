c9b39b7020e36c17998c243abc909b222bf89d05
Bruno Almeida <usuariodebruno@gmail.com>
2023-06-26 13:43:02 +0000

    Revert "removendo models Termos e seus rastros"
    This reverts commit 9516d861962eca287153618d19b20170e76a2ddf.

--- escambo_project/app_escambo/admin.py
+++ escambo_project/app_escambo/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Registro dos modelos no painel de administração
+admin.site.register(Termos)
 admin.site.register(Escambador)
 admin.site.register(Categoria)
 admin.site.register(Produto)
